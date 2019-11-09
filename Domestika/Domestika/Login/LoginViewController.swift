28db754bfa48695793d2b2a173742c6f4a407cd0
Enrique Blasco <e.blasco@artvisual.es>
2019-11-09 10:26:49 +0100

    make gradient view

--- Domestika/Domestika/Login/LoginViewController.swift
+++ Domestika/Domestika/Login/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  Domestika
 //
 //  Created by Enrique Blasco Blanquer on 09/11/2019.
@@ -8,13 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
 }
-
