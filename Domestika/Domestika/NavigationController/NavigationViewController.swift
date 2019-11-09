c42d22d503b5e63d5a43d67e28f059ccd7628fd8
Enrique Blasco <e.blasco@artvisual.es>
2019-11-09 11:45:37 +0100

    Set NavigationViewController

---
+++ Domestika/Domestika/NavigationController/NavigationViewController.swift
@@ -0,0 +1,18 @@
+//
+//  NavigationViewController.swift
+//  Domestika
+//
+//  Created by Enrique Blasco Blanquer on 09/11/2019.
+//  Copyright © 2019 eblasco. All rights reserved.
+//
+
+import UIKit
+
+class NavigationViewController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
