9d1e1b80d52d450df082e5b6d86d33d7c9aa3edd
Enrique Blasco <e.blasco@artvisual.es>
2019-11-11 00:01:56 +0100

    add forward/rewind the video 10 seconds

--- Domestika/Domestika/CourseDetail/CourseDetailViewController.swift
+++ Domestika/Domestika/CourseDetail/CourseDetailViewController.swift
@@ -12,12 +12,18 @@ import AVFoundation
 
 
 class CourseDetailViewController: UIViewController {
-
+    
     @IBOutlet weak var videoView: UIView!
     @IBOutlet weak var videoHeight: NSLayoutConstraint!
+    @IBOutlet weak var rewindBtn: UIButton!
+    @IBOutlet weak var forwardBtn: UIButton!
+    
     
     final let PERCENTOFVIDEO:CGFloat = 27
     var course:Course?
+    var player : AVPlayer!
+    var playerController:AVPlayerViewController?
+    let seekDuration: Float64 = 10
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,31 +31,69 @@ class CourseDetailViewController: UIViewController {
         playVideo()
     }
     
+    
     private func setViews(){
         self.navigationController?.setNavigationBarHidden(false, animated: true) //show navigationbar
         self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for:.default)
         self.navigationController?.navigationBar.shadowImage = UIImage()
         self.navigationController?.navigationBar.layoutIfNeeded()
-
+        
         let shareButton =   UIBarButtonItem(image: UIImage(named: "share"), style: .plain, target: self, action: #selector(share))
         self.navigationItem.rightBarButtonItem = shareButton
         
-        self.videoHeight.constant = (PERCENTOFVIDEO * UIScreen.main.bounds.height) / 100    }
+        self.videoHeight.constant = (PERCENTOFVIDEO * UIScreen.main.bounds.height) / 100
+    }
     
     private func playVideo(){
-        let player = AVPlayer(url: URL(string: self.course!.trailerUrl!)!)
-        let playerController:AVPlayerViewController? = AVPlayerViewController()
+        player = AVPlayer(url: URL(string: self.course!.trailerUrl!)!)
+        playerController = AVPlayerViewController()
         playerController!.player = player
         playerController!.view.frame = self.videoView.frame
         self.videoView.addSubview(playerController!.view)
         self.addChild(playerController!)
         player.play()
-        //player.seek(to: CMTime(seconds: 10.0, preferredTimescale:.zero))
+        videoView.bringSubviewToFront(rewindBtn)
+        videoView.bringSubviewToFront(forwardBtn)
+    }
+    
+    @IBAction func fastForwardBtn(_ sender: UIButton) {
+        
+        if player == nil { return }
+        if let duration  = player!.currentItem?.duration {
+            let playerCurrentTime = CMTimeGetSeconds(player!.currentTime())
+            let newTime = playerCurrentTime + seekDuration
+            if newTime < CMTimeGetSeconds(duration)
+            {
+                let selectedTime: CMTime = CMTimeMake(value: Int64(newTime * 1000 as Float64), timescale: 1000)
+                player!.seek(to: selectedTime)
+            }
+            player?.pause()
+            player?.play()
+        }
+        
+    }
+    
+    @IBAction func rewindBtn(_ sender: UIButton) {
+        
+        if player == nil
+        {
+            return
+        }
+        let playerCurrenTime = CMTimeGetSeconds(player!.currentTime())
+        var newTime = playerCurrenTime - seekDuration
+        if newTime < 0
+        {
+            newTime = 0
+        }
+        player?.pause()
+        let selectedTime: CMTime = CMTimeMake(value: Int64(newTime * 1000 as Float64), timescale: 1000)
+        player?.seek(to: selectedTime)
+        player?.play()
     }
     
     @objc func share(){
         
     }
- 
+    
     
 }
