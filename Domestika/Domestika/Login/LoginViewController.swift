7d5e73e56c6878d9dc75c95682c274018b2ff11d
Enrique Blasco <e.blasco@artvisual.es>
2019-11-11 06:41:16 +0100

    refactor login view

--- Domestika/Domestika/Login/LoginViewController.swift
+++ Domestika/Domestika/Login/LoginViewController.swift
@@ -9,8 +9,11 @@
 import UIKit
 import AVFoundation
 
+/**
+ Extra view. Screen that simulates a login with a background video and animated text.
+ */
 class LoginViewController: UIViewController {
-
+    
     @IBOutlet weak var lblOne: UILabel!
     @IBOutlet weak var lblTwo: UILabel!
     @IBOutlet weak var lblThree: UILabel!
@@ -25,6 +28,9 @@ class LoginViewController: UIViewController {
         startAnimation()
     }
     
+    /**
+     start the animation of the texts
+     */
     private func startAnimation(){
         lblOne.fadeIn(duration: 0.5, delay: 1.0, completion: {_ in
             self.lblTwo.fadeIn(duration: 0.5, delay: 0.0, completion: {_ in
@@ -34,37 +40,47 @@ class LoginViewController: UIViewController {
         })
     }
     
+    /**
+     play background video
+     */
     private func playBackgoundVideo() {
-    
+        
         let theURL = Bundle.main.url(forResource:"loginvideo", withExtension: "mp4")
-
+        
         avPlayer = AVPlayer(url: theURL!)
         avPlayerLayer = AVPlayerLayer(player: avPlayer)
         avPlayerLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
         avPlayer!.volume = 0
         avPlayer!.actionAtItemEnd = .none
-
+        
         avPlayerLayer.frame = view.layer.bounds
         view.backgroundColor = .clear
         view.layer.insertSublayer(avPlayerLayer, at: 0)
-
+        
+        //bucle video
         NotificationCenter.default.addObserver(self,
-                                           selector: #selector(playerItemDidReachEnd(notification:)),
-                                           name: NSNotification.Name.AVPlayerItemDidPlayToEndTime,
-                                           object: avPlayer?.currentItem)
+                                               selector: #selector(playerItemDidReachEnd(notification:)),
+                                               name: NSNotification.Name.AVPlayerItemDidPlayToEndTime,
+                                               object: avPlayer?.currentItem)
     }
     
+    /**
+     objc function, called when video finish
+     */
     @objc func playerItemDidReachEnd(notification: Notification) {
         let p: AVPlayerItem = notification.object as! AVPlayerItem
         p.seek(to: CMTime.zero, completionHandler: nil)
     }
     
+    /**
+     cicle live functions, start and pause background video
+     */
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         avPlayer!.play()
         paused = false
     }
-
+    
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
         avPlayer!.pause()
